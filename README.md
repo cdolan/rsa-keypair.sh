690b0606410b4d8e98c5c2fac007e7cf623c596d
Christopher Dolan <chris@codingstream.org>
2012-09-04 15:01:07 -0400

    Added X509 certificate generation

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
 # rsa-keypair.sh
 
-Simplifies generating an RSA key pair
+Simplifies generating an RSA key pair.
 
 ## Examples
 
-`sh ./rsa-keypair.sh -o mykey`
+The simpliest example:
 
-`sh ./rsa-keypair.sh -o mykey -b 2048`
+`sh ./rsa-keypair.sh mykey`
+
+If you want a really strong key:
+
+`sh ./rsa-keypair.sh -b 4096 mykey`
+
+To generate a key with an X509 certificate:
+
+`sh ./rsa-keypair.sh -c mykey`
+
+## Usage
+<pre>
+usage: sh ./rsa-keypair.sh [options] keyname
+
+Simplifies generating an RSA key pair.
+
+OPTIONS
+  -h    This usage information
+  -b    Bit length, defaults to 2048
+  -c    Generate associated X509 certificate
+  -e    Change certificate expiration, defaults to 365 days
+</pre>
--- rsa-keypair.sh
+++ rsa-keypair.sh
@@ -3,27 +3,35 @@
 OPENSSL=openssl
 KEYLEN=2048
 KEYNAME=
+CERT=
+EXPIRE=365
 
 function usage {
   cat << EOF
-  usage: $0 options
+  usage: $0 [options] keyname
   
   Simplifies generating an RSA key pair.
   
   OPTIONS
-    -o    Key name
+    -h    This usage information
     -b    Bit length, defaults to 2048
+    -c    Generate associated X509 certificate
+    -e    Change certificate expiration, defaults to 365 days
 EOF
 }
 
-while getopts "ho:b:" OPTION; do
+while getopts "hb:ce:" OPTION; do
   case $OPTION in
     h) usage ; exit 1   ;;
-    o) KEYNAME=$OPTARG  ;;
     b) KEYLEN=$OPTARG   ;;
+    c) CERT=1           ;;
+    e) EXPIRE=$OPTARG   ;;
     ?) usage ; exit 1   ;;
   esac
 done
+
+shift $(($OPTIND - 1))
+KEYNAME=$@
 
 if [ -z $KEYNAME ]; then
   usage
@@ -32,5 +40,9 @@ fi
 
 $OPENSSL genrsa -out $KEYNAME.pem $KEYLEN
 $OPENSSL rsa -in $KEYNAME.pem -pubout > $KEYNAME.pub
+
+if [ $CERT ]; then
+  $OPENSSL req -new -x509 -key $KEYNAME.pem -out $KEYNAME.cert.pem -days $EXPIRE
+fi
 
 exit
